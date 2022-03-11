d017aaf7c0f18f64ebe53c1d768567d036eb5b62
Rohit Pandey <rohitpandey498@gmail.com>
2022-03-11 19:26:17 +0530

    remove authentication

--- send_mail/settings.py
+++ send_mail/settings.py
@@ -130,7 +130,7 @@ DEFAULT_FROM_EMAIL = 'rohitpandey498@gmail.com'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rohitpandey498@gmail.com'
-EMAIL_HOST_PASSWORD = 'rohit20051996'
+EMAIL_HOST_USER = 'abc@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxx'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
